481af65e5dd7d4087014dd84a54cb31b2da3cfc6
Juyeong Maing <mjy9088@naver.com>
2026-01-22 14:04:05 +0900

    initial build

---
+++ fxmanifest.lua
@@ -0,0 +1,83 @@
+fx_version 'cerulean'
+game 'gta5'
+
+ui_page 'html/index.html'
+
+client_scripts {
+	client/**,
+}
+
+server_scripts {
+  	@oxmysql/lib/MySQL.lua,
+	server/**,
+}
+
+shared_scripts {
+  	@ox_lib/init.lua,
+	config/**,
+}
+
+files {
+    'html/assets/DailyMotion-C2Fpo8rG.js',
+    'html/assets/Facebook-BpN2nuCV.js',
+    'html/assets/FilePlayer-BrYEFj7P.js',
+    'html/assets/howler-m_Qlu3oy.js',
+    'html/assets/index-C9YnOZvp.js',
+    'html/assets/index-DrdrNGg2.css',
+    'html/assets/Kaltura-D8jsmjSE.js',
+    'html/assets/Mixcloud-CYQhLzd_.js',
+    'html/assets/Mux-PG0NHnyv.js',
+    'html/assets/Preview-DAJ9Sp09.js',
+    'html/assets/SoundCloud-CisZhrPb.js',
+    'html/assets/Streamable-DJ4ZqHbJ.js',
+    'html/assets/Twitch-Zju_LlY8.js',
+    'html/assets/Vidyard-ZufHdC6B.js',
+    'html/assets/Vimeo-BCh7qPrk.js',
+    'html/assets/Wistia-CQBGTfnE.js',
+    'html/assets/YouTube-CLcqcauq.js',
+    'html/frames/1.svg',
+    'html/frames/2.svg',
+    'html/frames/3.svg',
+    'html/frames/4.svg',
+    'html/images/ads.svg',
+    'html/images/anonim.jpg',
+    'html/images/bg.png',
+    'html/images/camera.svg',
+    'html/images/contact.svg',
+    'html/images/default-avatar.png',
+    'html/images/gallery.svg',
+    'html/images/garage.svg',
+    'html/images/houses.svg',
+    'html/images/inetmax.svg',
+    'html/images/iphone-15pro-ori.png',
+    'html/images/iphone-top.svg',
+    'html/images/kota-v.png',
+    'html/images/loops.svg',
+    'html/images/loops_default_cover.jpg',
+    'html/images/loops-signin-bg.svg',
+    'html/images/loops-signup-bg.svg',
+    'html/images/loops-tweet.svg',
+    'html/images/loops-white.svg',
+    'html/images/lovy.svg',
+    'html/images/mail.svg',
+    'html/images/message.svg',
+    'html/images/monas.png',
+    'html/images/news.svg',
+    'html/images/noimage.jpg',
+    'html/images/notif.svg',
+    'html/images/phone.svg',
+    'html/images/playtv.svg',
+    'html/images/services.svg',
+    'html/images/setting.svg',
+    'html/images/signal/0.svg',
+    'html/images/signal/1.svg',
+    'html/images/signal/2.svg',
+    'html/images/signal/3.svg',
+    'html/images/signal/4.svg',
+    'html/images/wallet.svg',
+    'html/images/x.svg',
+    'html/index.html',
+    'html/sounds/call-sound.mp3',
+    'html/sounds/message-sound.mp3',
+    'html/static/config.json',
+}
